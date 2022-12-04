d373c54925b0660448097bb3e52d642cf52096bf
carrobader <carolinebader@hotmail.com>
2022-12-04 22:16:11 +0100

    Add series code

--- App.js
+++ App.js
@@ -1,50 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, SafeAreaView, FlatList, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { AntDesign, Octicons, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import RNPickerSelect from 'react-native-picker-select';
-let movieList = require( "./movies");
-
-
+import { StatusBar } from 'expo-status-bar'
+import {
+  ImageBackground,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {
+  AntDesign,
+  Octicons,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons'
+import RNPickerSelect from 'react-native-picker-select'
+let movieList = require('./movies')
+let seriesList = require('./series')
+let watchedEpisodes = []
+let seasonAndEpisode = {}
 
 const styles = StyleSheet.create({
   playButton: {
-    borderRadius: "25%",
+    borderRadius: '25%',
     width: 260,
     height: 120,
     marginLeft: 30,
-    backgroundColor: "#3e3e3e",
+    backgroundColor: '#3e3e3e',
     marginBottom: 20,
   },
   remoteIcon: {
-    display: "flex",
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: "auto",
-    marginBottom: "auto"
+    display: 'flex',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
   },
   remoteButton: {
     marginLeft: 30,
     marginRight: 30,
     marginTop: 20,
     marginBottom: 20,
-    backgroundColor: "#3e3e3e",
-    borderRadius: "50%",
-    display: "flex",
+    backgroundColor: '#3e3e3e',
+    borderRadius: '50%',
+    display: 'flex',
     width: 100,
     height: 100,
   },
   remoteRow: {
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   remoteContainer: {
     display: 'flex',
     flexWrap: 'wrap',
     flexDirection: 'column',
     padding: 20,
-    backgroundColor: '#141414',    
+    backgroundColor: '#141414',
   },
   container: {
     flex: 1,
@@ -63,9 +78,9 @@ const styles = StyleSheet.create({
     marginRight: 20,
     textTransform: 'uppercase',
     textAlign: 'center',
-    color: "#ebebeb",
+    color: '#ebebeb',
     fontWeight: 'bold',
-    textShadowColor: "#141414",
+    textShadowColor: '#141414',
     textShadowRadius: 5,
     marginBottom: 15,
   },
@@ -75,16 +90,16 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     marginTop: -20,
     marginLeft: -17,
-    color: "#ebebeb",
+    color: '#ebebeb',
     fontWeight: 'bold',
-    textShadowColor: "#141414",
+    textShadowColor: '#141414',
     textShadowRadius: 5,
     marginBottom: 15,
   },
   button: {
-    width: "100%",
-    justifyContent: "center",
-    display: "flex",
+    width: '100%',
+    justifyContent: 'center',
+    display: 'flex',
     color: '#000',
   },
   item: {
@@ -95,10 +110,10 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     margin: 10,
   },
-});
+})
 
 const customPickerStyles = StyleSheet.create({
   inputIOS: {
@@ -120,48 +135,198 @@ const customPickerStyles = StyleSheet.create({
     color: 'black',
     paddingRight: 30, // to ensure the text is never behind the icon
   },
-});
+})
 
 export default function App() {
   const [category, setCategory] = useState(null)
   const [categories, setCategories] = useState([])
-  const [country, setCountry] = useState("Sweden")
+  const [typeChoice, setTypeChoice] = useState(['Movies'])
+  const [country, setCountry] = useState('Sweden')
   const [movies, setMovies] = useState([])
+  const [series, setSeries] = useState([])
+  const [netflixIdea, setnetflixIdea] = useState(null)
   const [showRemote, setShowRemote] = useState(false)
+  const [seasonEpisodeData, setSeasonEpisodeData] = useState(null)
 
-
-  const openURL = (netflix_id) => {
-    console.log("OPENURL")
+  const openURL = (netflixId) => {
+    setnetflixIdea(netflixId)
+    console.log('OPENURL')
+    getSerieData(netflixId)
     setShowRemote(true)
-    fetch(`http://192.168.8.152:5000/${netflix_id}`)
+    fetch(`http://192.168.8.152:5000/${netflixId}`)
   }
-  
+
   const keyCommand = (key) => {
-    if (key == "close") {
+    if (key == 'close') {
       setShowRemote(false)
     }
     fetch(`http://192.168.8.152:5000/key-command/${key}`)
   }
-  
-  const Item = ({ title, img, netflixId }) => (
-    <ImageBackground source={{uri: img}} resizeMode="cover" style={styles.image}>
-      <View style={{padding: 20, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.25)"}}>
-      <Text style={styles.title}>{title}</Text>
 
-      <TouchableOpacity onPress={() => openURL(netflixId)} style={styles.button}><AntDesign style={{alignSelf: "center", marginBottom: 10, textShadowColor: "rgba(0, 0, 0, 0.71)", textShadowRadius: 10, padding: 15}} name="playcircleo" size={80} color="white" /></TouchableOpacity>
+  const toggleTitle = (typeChoice) => {
+    return { Movies: 'Series', Series: 'Movies' }[typeChoice]
+  }
+
+  const getASeason = (seasons) => {
+    return Math.floor(Math.random() * seasons + 1)
+  }
+
+  const getAnEpisode = (episodes) => {
+    return Math.floor(Math.random() * episodes + 1)
+  }
+
+  const getRandomEpisode = (seasonEpisodes) => {
+    if (Object.keys(seasonAndEpisode).length === 0)
+      seasonAndEpisode = createSeasonEpisodeData(seasonEpisodes)
+
+    let seasonEpisodeObject = createSeasonEpisodeObject(seasonAndEpisode)
+
+    if (watchedEpisodes.length === 0) {
+      watchedEpisodes.push(seasonEpisodeObject)
+      openURLSerie()
+    } else {
+      let exists = checkIfValueExists(seasonEpisodeObject)
+
+      if (exists) {
+        getRandomEpisode('')
+      } else {
+        watchedEpisodes.push(seasonEpisodeObject)
+        openURLSerie()
+      }
+
+      console.log('AFTER WATCH:', watchedEpisodes)
+    }
+  }
+
+  const openURLSerie = () => {
+    console.log('OPENURLAerie')
+    fetch(`http://172.20.10.2:5000/70196252`)
+    // fetch(`http://172.20.10.2:5000/${netflixIdea}`)
+  }
+
+  const createSeasonEpisodeData = (seasonEpisodes) => {
+    let seasonsAndEpisodes = {}
+
+    seasonEpisodesData = seasonEpisodes.replaceAll('S', '').split(',')
+
+    seasonEpisodesData.forEach((element) => {
+      new_element = element.split(':')
+      seasonsAndEpisodes[new_element[0]] = new_element[1]
+    })
+
+    return seasonsAndEpisodes
+  }
+
+  const createSeasonEpisodeObject = (seasonAndEpisode) => {
+    season = getASeason(Object.keys(seasonAndEpisode).length)
+    episode = getAnEpisode(seasonAndEpisode[season])
+
+    return { season: season, episode: episode }
+  }
+
+  const checkIfValueExists = (seasonEpisodeObject) => {
+    for (let variable of watchedEpisodes) {
+      if (
+        variable.season === seasonEpisodeObject.season &&
+        variable.episode === seasonEpisodeObject.episode
+      ) {
+        return true
+      }
+    }
+
+    return false
+    // for (let i = 0; i < watchedEpisodes.length; i++) {
+    //   // console.log('watchedEpisodes', watchedEpisodes[i], i)
+    //   // console.log('seasonEpisodeObject', seasonEpisodeObject, i)
+    //   if (
+    //     watchedEpisodes[i].season === seasonEpisodeObject.season &&
+    //     watchedEpisodes[i].episode === seasonEpisodeObject.episode
+    //   ) {
+    //     console.log('if')
+    //     getRandomEpisode('')
+    //     // console.log('SEASONEPISODEOBJECT:', seasonEpisodeObject)
+    //     // watchedEpisodes.push(seasonEpisodeObject)
+    //     // break
+    //   }
+    // }
+  }
+
+  const getSerieData = (netflixId) => {
+    // const options = {
+    //   method: 'GET',
+    //   url: 'https://unogs-unogs-v1.p.rapidapi.com/title/countries',
+    //   params: { netflix_id: netflixId },
+    //   headers: {
+    //     'X-RapidAPI-Key': '876d91601bmsh299991229f50559p1bfe00jsn9cfb26a0fe9a',
+    //     'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com',
+    //   },
+    // }
+
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     let countries = response.data.results
+
+    //     countries.forEach((element) => {
+    //       if (element.country_code === 'SE') {
+    //         setSeasonEpisodeData(element.season_detail)
+    //       }
+    //     })
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error)
+    //   })
+
+    // setSeasonEpisodeData('S1:7,S2:13,S3:13,S4:13,S5:16')
+    setSeasonEpisodeData('S1:3')
+  }
+
+  const Item = ({ title, img, netflixId }) => (
+    <ImageBackground
+      source={{ uri: img }}
+      resizeMode="cover"
+      style={styles.image}
+    >
+      <View
+        style={{
+          padding: 20,
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgba(0, 0, 0, 0.25)',
+        }}
+      >
+        <Text style={styles.title}>{title}</Text>
+
+        <TouchableOpacity
+          onPress={() => openURL(netflixId)}
+          style={styles.button}
+        >
+          <AntDesign
+            style={{
+              alignSelf: 'center',
+              marginBottom: 10,
+              textShadowColor: 'rgba(0, 0, 0, 0.71)',
+              textShadowRadius: 10,
+              padding: 15,
+            }}
+            name="playcircleo"
+            size={80}
+            color="white"
+          />
+        </TouchableOpacity>
       </View>
     </ImageBackground>
-  );
+  )
   useEffect(() => {
     const options = {
       method: 'GET',
       url: 'https://unogs-unogs-v1.p.rapidapi.com/static/genres',
       headers: {
         'X-RapidAPI-Key': '6f882fb5b5mshb29ecdaca69481fp111c6ajsn4577c25cf140',
-        'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
-      }
-    };
-    
+        'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com',
+      },
+    }
+
     /*axios.request(options).then(function (response) {
       const result = response.data.results
       const items = []
@@ -177,66 +342,178 @@ export default function App() {
     }).catch(function (error) {
       console.error(error);
     });*/
-    const c = [{
-      value: 12739,
-      label: "20th Century Period Pieces"
-    },
-    {
-      value: 77213,
-      label: "Absurd Comedies"
-    }]
+    const c = [
+      {
+        value: 12739,
+        label: '20th Century Period Pieces',
+      },
+      {
+        value: 77213,
+        label: 'Absurd Comedies',
+      },
+    ]
     setCategories(c)
     setCategory(c[0].value)
   }, [])
- const Dropdown = () => {
+
+  const Dropdown = () => {
     return (
-      <View style={{width: "95%", marginLeft: "auto", marginRight: "auto"}}>
-      <Text style={styles.category}>{category && categories && categories.find(element => element.value == category).label}</Text>
-
-      <View style={{display: "flex", flexDirection: "col", height: 60}}>
-
-        <RNPickerSelect
-            style={customPickerStyles}
-            placeholder={{label: "Choose genre", value: ""}}
-            onValueChange={(value) => setCategory(value)}
-            items={categories}
-        ></RNPickerSelect>
-        <AntDesign style={{ marginTop: -28, marginLeft: "auto", marginRight: 20}} name="caretdown" size={14} color="white" />
-        </View>
-        </View>
-    );
-  };
+      <View style={{ width: '95%', marginLeft: 'auto', marginRight: 'auto' }}>
+        <Text
+          style={styles.category}
+          onPress={() => setTypeChoice(toggleTitle(typeChoice))}
+        >
+          {typeChoice}
+        </Text>
+        {typeChoice === 'Movies' ? (
+          <View style={{ display: 'flex', flexDirection: 'col', height: 60 }}>
+            <RNPickerSelect
+              style={customPickerStyles}
+              placeholder={{ label: 'Choose genre', value: '' }}
+              onValueChange={(value) => setCategory(value)}
+              items={categories}
+            ></RNPickerSelect>
+            <AntDesign
+              style={{ marginTop: -28, marginLeft: 'auto', marginRight: 20 }}
+              name="caretdown"
+              size={14}
+              color="white"
+            />
+          </View>
+        ) : (
+          <Text></Text>
+        )}
+      </View>
+    )
+  }
 
   const Remote = () => {
     return (
       <View style={styles.remoteContainer}>
         <View style={styles.remoteRow}>
-      <TouchableOpacity style={styles.playButton}onPress={() => keyCommand("space")} ><MaterialCommunityIcons name="play-pause" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-
+          <TouchableOpacity
+            style={styles.playButton}
+            onPress={() => keyCommand('space')}
+          >
+            <MaterialCommunityIcons
+              name="play-pause"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+          {/* <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => getRandomEpisode(seasonEpisodeData)}
+          >
+            <MaterialIcons
+              name="shuffle-on"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity> */}
         </View>
-       
-        <View style={styles.remoteRow}>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("up")} ><MaterialIcons name="volume-up" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("down")} ><MaterialIcons name="volume-down" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-      </View>
-      <View style={styles.remoteRow}>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("f")} ><Octicons name="screen-full" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("m")} ><MaterialIcons name="volume-off" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
 
-      </View>
         <View style={styles.remoteRow}>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("rewind")} ><MaterialIcons name="replay-10" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("fast-forward")} ><MaterialIcons name="forward-10" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-      
-      </View>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('up')}
+          >
+            <MaterialIcons
+              name="volume-up"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('down')}
+          >
+            <MaterialIcons
+              name="volume-down"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+        </View>
         <View style={styles.remoteRow}>
-        <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("refresh")} ><MaterialIcons name="refresh" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
-      <TouchableOpacity style={styles.remoteButton}onPress={() => keyCommand("close")} ><MaterialIcons name="close" size={60} color="white"  style={styles.remoteIcon}/></TouchableOpacity>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('f')}
+          >
+            <Octicons
+              name="screen-full"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('m')}
+          >
+            <MaterialIcons
+              name="volume-off"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.remoteRow}>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('rewind')}
+          >
+            <MaterialIcons
+              name="replay-10"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('fast-forward')}
+          >
+            <MaterialIcons
+              name="forward-10"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.remoteRow}>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('refresh')}
+          >
+            <MaterialIcons
+              name="refresh"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.remoteButton}
+            onPress={() => keyCommand('close')}
+          >
+            <MaterialIcons
+              name="close"
+              size={60}
+              color="white"
+              style={styles.remoteIcon}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-     </View>
     )
   }
-  
 
   useEffect(() => {
     /*const options = {
@@ -261,27 +538,40 @@ export default function App() {
     }).catch(function (error) {
       console.error(error);
     });*/
-      setMovies(movieList[category])
+    setMovies(movieList[category])
   }, [category])
+
+  useEffect(() => {
+    setSeries(seriesList)
+  })
 
   const renderItem = ({ item }) => (
     <Item title={item.title} img={item.img} netflixId={item.netflix_id} />
-  );
+  )
 
   return (
     <SafeAreaView style={styles.container}>
-      {!showRemote ?
-      <View>
-      <Dropdown/>
-      <FlatList
-        data={movies}
-        renderItem={renderItem}
-        keyExtractor={item => item.netflix_id}
-      />
-     </View> 
-      : <Remote/>}
+      {!showRemote ? (
+        <View>
+          <Dropdown />
+          {typeChoice === 'Movies' ? (
+            <FlatList
+              data={movies}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.netflix_id}
+            />
+          ) : (
+            <FlatList
+              data={series}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.netflix_id}
+            />
+          )}
+        </View>
+      ) : (
+        <Remote />
+      )}
       <StatusBar style="auto" />
     </SafeAreaView>
-  );
+  )
 }
-
--- server/app.py
+++ server/app.py
@@ -3,31 +3,36 @@ import webbrowser
 from pynput.keyboard import Key, Controller
 from pynput.mouse import Button, Controller as Mouse
 from time import sleep
-import pyautogui
+from platform import system
 
 app = Flask(__name__)
+print(system)
+ctrl = Key.ctrl if system() != "Darwin" else Key.cmd
 keys = {
-        "space": [Key.space],
-        "up": [Key.up],
-        "down": [Key.down],
-        "rewind": [Key.shift, Key.left],
-        "fast-forward": [Key.shift, Key.right],
-        "close": [Key.ctrl, "w"],
-        "refresh": [Key.ctrl, "r"]
-        }
+    "space": [Key.space],
+    "up": [Key.up],
+    "down": [Key.down],
+    "rewind": [Key.shift, Key.left],
+    "fast-forward": [Key.shift, Key.right],
+    "close": [ctrl, "w"],
+    "refresh": [ctrl, "r"],
+}
 mouse = Mouse()
+
 
 def play_on_start():
     sleep(5)
     mouse.position = (987, 484)
     sleep(0.2)
     for i in range(1):
-         mouse.press(Button.left)
-         mouse.release(Button.left)
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+
 
 @app.route("/")
 def index():
     return "START"
+
 
 @app.route("/<netflix_id>")
 def open_site(netflix_id):
@@ -35,6 +40,7 @@ def open_site(netflix_id):
     webbrowser.open(url, new=1, autoraise=True)
     play_on_start()
     return "STARTED"
+
 
 @app.route("/key-command/<key>")
 def key_command(key):
@@ -57,6 +63,6 @@ def key_command(key):
         play_on_start()
     return "Success"
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
-
